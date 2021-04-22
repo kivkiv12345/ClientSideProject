4f9d66b70aac8adae402ddd00aeeb3a872fca36b
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2021-04-22 19:47:43 +0200

    Add project files.

---
+++ ClientSideProject2/Scripts/MakeNavBar.js
@@ -0,0 +1,30 @@
+﻿function MakeTopNavBar() {
+    $("#TopNavigationBar").load("navbar.html");
+
+    var CurrentPage;
+
+    try {
+        CurrentPage = document.location.href.match(/[^\/]+$/)[0];
+    }
+    catch (err) {
+        CurrentPage = "index.html";
+    }
+
+    setTimeout(function () {
+
+        // Gets the <li> tags inside the <ul> tag which is inside the <nav> tag inside the "TopNavigationBar" <div>...
+        listelements = document.getElementById("TopNavigationBar").children[0].children[0].children;
+
+        Array.prototype.forEach.call(listelements, function (li, index) {
+            if (li.children[0].getAttribute("href") == CurrentPage)
+                li.style.backgroundColor = "limegreen";
+        })
+
+        // JQuery solution.
+        /*$("#TopNavigationBar").find('a').each(function () {
+            if ($(this).attr('href') == CurrentPage) {
+                $(this)[0].parentElement.style.backgroundColor = "limegreen";
+            }
+        })*/
+    }, 50);
+}
